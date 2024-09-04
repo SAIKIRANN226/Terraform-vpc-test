1e361ad95953366661def0c9f8a6ee9647174f6c
mothukuru saikiran <obscuresai.com>
2024-09-04 15:54:50 +0530

    message

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ module "roboshop" {
   common_tags = var.common_tags
   vpc_tags = var.vpc_tags
 
+  # the above line from 3 to 6 are mandatory to pass here
+
   # public subnet
   public_subnets_cidr = var.public_subnets_cidr
 

5921b7d5026a799c2dfa1ee8312d24227d5e2460
mothukuru saikiran <obscuresai.com>
2024-09-10 17:10:15 +0530

    message

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 module "roboshop" {
+  #source = "git"::https://github.com/daws-76s/terraform-aws-vpc.git?ref=main"  this means referring from the open source which is is internet
   source = "../tterraform-aws-vpc"
   project_name = var.project_name
   environment = var.environment
   common_tags = var.common_tags
   vpc_tags = var.vpc_tags
 
-  # the above line from 3 to 6 are mandatory to pass here
+  # the above line from 3 to 6 are mandatory to pass to module code and it will work in "terraform-aws-vpc"
 
   # public subnet passing here is mandatory
   public_subnets_cidr = var.public_subnets_cidr
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "azs" {
-  value = module.roboshop.azs
+  value = module.roboshop.azs  # here it will print all the AZ's so that we can able to know that AZ's are fetching with "names" as output use this names in locals line 3
 }
